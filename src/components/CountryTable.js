2b1d9763f81dfe2a9cc11f7a0e96c2e6decf106f
Rayatul Islam <rayatislam@gmail.com>
2021-10-10 13:38:30 +0200

    API changed

--- src/components/CountryTable.js
+++ src/components/CountryTable.js
@@ -1,74 +1,57 @@
-import React from 'react'
+import React from "react";
 
-export const CountryTable = ({countries}) => {
-
+export const CountryTable = ({ id, title, url }) => {
   return (
     <>
-      {countries.length === 0
-        && (
-          <div className="loading">
-            {'Countries are loading...'}
-          </div>)}
-      {countries.length >= 1
-        && (
-      <>
-        {countries.map((country) => {
-            const {numericCode,name,area,population,region} =  country
+      <div key={id} className="itemBox row">
+        <div className="col-md-4">
+          <img src={url} alt={title} />
+        </div>
+        <div className="col-md-8">
+          <h3>{title}</h3>
 
-            return (
-              <div key={numericCode} className="itemBox row">
-	
-							<div className="col-md-4">
-								 
-							</div>
-							<div className="col-md-8">
+          <p>
+            <strong>City/Locality: </strong>
+            <span className="orange-text-color"> {title.substr(0, 12)}</span>
+          </p>
 
-								<h3>{name}</h3>
-							
-								<p><strong>City/Locality:</strong> <span className="orange-text-color">{region}</span></p>
-		
-								<p><strong>Total Capacity:</strong> <span className="orange-text-color">{Math.round(area * 0.3861)}</span></p>
-		
-								<p><strong>Mobile Number:</strong> <span className="orange-text-color">22700007</span></p>
-		
-								<p><strong>Pincode:</strong> <span className="orange-text-color">400001</span></p>
-		
-								<p><strong>Address:</strong> 17, Shri S.R. Marg, Ballard Estate, Fort, Mumbai, Maharashtra</p>
-								
-								<a  data-toggle="collapse" href="#details-box1" role="button" aria-expanded="false" aria-controls="details-box1" className="btn btn-primary">More Details</a>						
-								
-								<div className="collapse">
-									<div className="card card-body">
-									  <h4>Specialities</h4>
-									  <p>Dental , Dermatology, Emergency medicine, Obstetrics & gynaecology, Paedriatrics & child care, General medicine, Radiology, General surgery, Venereology/venereal diseases/STDs, Orthopedics, Ear nose throat (ENT), Leprosy clinic, HIV/AIDS clinic</p>
+          <p>
+            <strong>Total Capacity:</strong>{" "}
+            <span className="orange-text-color">{id}</span>
+          </p>
 
-									  <h4>Health Facilites</h4>
-									  <p>Only OPD, Both IPD and OPD, Only lab, Diagnostic</p>
-									  
-									  <h4>Email</h4>
-									  <p>any@any.com</p>
+          <p>
+            <strong>Mobile Number:</strong>{" "}
+            <span className="orange-text-color">{Math.round(id * 59)}</span>
+          </p>
 
-									  <h4>Phone</h4>
-									  <p>0000000000</p>
+          <p>
+            <strong>Pincode:</strong>{" "}
+            <span className="orange-text-color">
+              {id}
+              {id}
+              {id}
+              {id}
+            </span>
+          </p>
 
-									  <h4>Ownership</h4>
-									  <p>Public</p>
+          <p>
+            <strong>Address:</strong> 17, Shri S.R. Marg, Ballard Estate, Fort,
+            Mumbai, Maharashtra
+          </p>
 
-
-									</div>
-								  </div>
-
-							</div>
-
-
-	
-						</div>
-
-                  
-            )
-          })}        
-      </>)}
+          <a
+            data-toggle="collapse"
+            href="#details-box1"
+            role="button"
+            aria-expanded="false"
+            aria-controls="details-box1"
+            className="btn btn-primary"
+          >
+            More Details
+          </a>
+        </div>
+      </div>
     </>
-  )
-}
-
+  );
+};
--- src/containers/Body.js
+++ src/containers/Body.js
@@ -1,83 +1,47 @@
-import React, { Component} from "react"
-import axios from 'axios'
-import { CountryTable } from '../components/CountryTable'
-import { LanguageTable } from '../components/LanguageTable'
+import React, { Component, useState, useEffect } from "react";
+import axios from "axios";
+import { CountryTable } from "../components/CountryTable";
+import { LanguageTable } from "../components/LanguageTable";
 
 const APIS = {
-  CountryAPI: 'https://restcountries.eu/rest/v2/all'
-}
+  CountryAPI: "https://restcountries.eu/rest/v2/all",
+};
 
-export default class Body extends Component {
- state = {
-   countries: []
- }
+const Body = () => {
+  const [todos, setTodos] = useState([]);
 
-
- async getData(APIS) {
-   this.setState((prevState, props) => ({
-     countries: [],
-   }))
-   try{
-     const response = await axios.get(APIS,
-       {
-         header:
-           {'Content-Type': 'application/json'}
-         })
-       return response
-   } catch(error) {
-     console.log('Error:', error)
-     return []
-   }
- }
-
-  componentDidMount() {
-   var initPromise = this.getData(APIS.CountryAPI)
-   let names
-   initPromise
-   .then(countries => (
-     names = countries.data.map(country => country.name),
-     this.setState((prevState, props) => ({
-       countries: countries.data,
-       names,
-     }))
-   ))
- }
- 
- render(){
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/photos").then(
+      async (response) => {
+        if (response.ok) setTodos(await response.json());
+        console.log("REEEEEEEEEEEEEE", todos);
+      }
+    );
+  }, []);
 
   return (
     <>
+      <section id="container" className="container">
+        <div className="row">
+          <div className="col-md-12">
+            <h2>Available Beds</h2>
 
-<section id="container" className="container">
-			
-			<div className="row">
-				<div className="col-md-12">
-					<h2>Available Beds</h2>
+            <select className="sort-select">
+              <option>Sort By</option>
+              <option>Number of beds</option>
+              <option>Availability</option>
+            </select>
 
-					<select className="sort-select">
-						<option>Sort By</option>
-						<option>Number of beds</option>
-						<option>Availability</option>
-					</select>
-						
-					<div  className="commodityWrap">
-	
-          <CountryTable countries={this.state.countries}/>
-	
-					</div>
-	
-
-				</div>
-			</div>	
-			
-
-		</section>
-
-
-    
+            <div>
+              {todos.slice(0, 20).map((item) => (
+                <CountryTable id={item.id} title={item.title} url={item.url} />
+              ))}
+            </div>
+          </div>
+        </div>
+      </section>
     </>
-  )
-   }
-}
+  );
+};
 
-
+export default Body;
